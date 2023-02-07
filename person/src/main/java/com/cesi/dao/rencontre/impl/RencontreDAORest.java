7b4b8d5ea621325b7bed0533c22c18b6a4e64e55
Pauline LE MOING <lmg.pauline1@gmail.com>
2023-02-07 10:47:48 +0000

    c chiant

--- person/src/main/java/com/cesi/dao/rencontre/impl/RencontreDAORest.java
+++ person/src/main/java/com/cesi/dao/rencontre/impl/RencontreDAORest.java
@@ -2,17 +2,22 @@ package com.cesi.dao.rencontre.impl;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
 
 import com.cesi.controller.rencontre.model.Rencontre;
 import com.cesi.dao.rencontre.RencontreDAO;
-import com.cesi.dao.rencontre.model.RencontreDTO;
 
 @Repository //veut dire qu'on est une dao 
 public class RencontreDAORest implements RencontreDAO {
 
-    public RencontreDAORest(){
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public RencontreDAORest(RestTemplate restTemplate){
+    this.restTemplate=restTemplate;
     }
 
     @Override
@@ -25,8 +30,12 @@ public class RencontreDAORest implements RencontreDAO {
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
         HttpEntity<Rencontre> entity = new HttpEntity<Rencontre>(rencontre, headers);
+        response = restTemplate.exchange(url, HttpMethod.POST, entity, Rencontre.class);
 
-        return null;
+        Rencontre retour = response.getBody();
+        
+        return retour;
+
     }
 
 
--- person/src/main/java/com/cesi/person/PersonApplication.java
+++ person/src/main/java/com/cesi/person/PersonApplication.java
@@ -2,7 +2,9 @@ package com.cesi.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
